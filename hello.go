12b81f931bb1cabb122aba070034163f267bc281
Julie Hodne Gundersen <jhg1997@gmail.com>
2018-01-25 16:12:10 +0100

    Julie

--- hello.go
+++ hello.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
-	fmt.Println("Maren Myhre")
-	fmt.Println("Julie Hodne Gundersen ")
-	fmt.Println("Katrine Skomedal")
+	fmt.Println("Hello there")
+	fmt.Println("Sophie Kleiva")
 
 }
