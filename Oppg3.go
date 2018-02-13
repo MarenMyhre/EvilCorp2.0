4a2c80ec9c575efcf215e1659e6457d6a1875869
Julie Hodne Gundersen <jhg1997@gmail.com>
2018-02-13 15:08:45 +0100

    Julie

--- Oppg3.go
+++ Oppg3.go
@@ -10,13 +10,13 @@ import "syscall"
 func main () {
 
 	sign := make(chan os.Signal, 1)
-	quitCh = make(chan bool, 1)
+	quitCh = make(chan int)
 
 	signal.Notify(sign, syscall.SIGINT, syscall.SIGTERM)
 
 	for i := 0; 1 < 100; i++ {
 		fmt.Println("Hello world")
-	}
+	{
 case syscall.SIGINT:
 fmt.Println("Stop")
 
