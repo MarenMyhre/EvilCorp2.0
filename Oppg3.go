11b4924133480019c404c8cfc389635a1c610056
Julie Hodne Gundersen <jhg1997@gmail.com>
2018-02-13 14:02:39 +0100

    Julie

--- Oppg3.go
+++ Oppg3.go
@@ -1,12 +1,34 @@
 package main
 
 
-import ("fmt"
+import "fmt"
+import "os"
+import "os/signal"
+import "syscall"
 
-)
 
-func main (){
-	for i:=0; 1 < 100; i++{
+func main () {
+
+	sign := make(chan os.Signal, 1)
+	quitCh = make(chan bool, 1)
+
+	signal.Notify(sign, syscall.SIGINT, syscall.SIGTERM)
+
+	for i := 0; 1 < 100; i++ {
 		fmt.Println("Hello world")
 	}
+case syscall.SIGINT:
+fmt.Println("Stop")
+
+case syscall.SIGTERM:
+fmt.Println("Terminate")
+quitCh <- 0
+
+default:
+fmt.Println("Unknown")
+quitCh <- 1
+
+
+code := quitCh
+os.Exit(code)
 }
