02a4e877f995a42e7c0f7b1794d04537930e1936
SophieKleiva <35769007+SophieKleiva@users.noreply.github.com>
2018-02-14 15:00:16 +0100

    Update ascii.go

--- OppgaveFire/ascii.go
+++ OppgaveFire/ascii.go
@@ -1,85 +1,33 @@
 package ascii
 
-import "fmt"
+import ("fmt"
+	)
+
+const Ascii = "\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f" +
+	"\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f" +
+	"\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf" +
+	"\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf" +
+	"\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf" +
+	"\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf" +
+	"\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef" +
+	"\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
 
 
-const Ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
-	"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f" +
-	` !"#$%&'()*+,-./0123456789:;<=>?` +
-	`@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_` +
-	"`abcdefghijklmnopqrstuvwxyz{|}~\x7f" + "\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92" +
-	"\x93\x94\x95\x96\x97\x98\x99\x9A\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6" +
-	"\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9" +
-	"\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC" +
-	"\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD8\xD9\xDA\xDB\xDC\xDD\xDE" +
-	"\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1" +
-	"\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF"
-
-func  IterateOverExtendedASCIIStringLiteral( sl string) {
-	for i := 0; i < len(sl) ; i++{
-		fmt.Printf("%X %c %b \n", sl[i], sl[i], sl[i])
-
+func IterateOverASCIIStringLiteral(sl string) {
+	for i := 0; i < len(sl); i++ {
+		fmt.Printf("%X %q %b\n", sl[i], sl[i], sl[i])
 	}
 
+	// For øyeblikket vises ikke tegnene mellom byte  0x80 til 0x95. Det er fordi våre PCer ikke har tilgang på disse
+	// tegnene. Dette kan fikses med å implementere runer inn i koden, men det har ikke vi gjort
 }
 
-func cp1252ToUTF8(cp string) string {
-	r := make([]rune, len(cp))
-	for i := 0; i < len(cp); i++ {
-		r[i] = cp1252[cp[i]]
-	}
-	return string(r)
-}
+// Oppgave 4b
+const Sekvens  = "\xF1\xf7\xbe dollar"
 
-func ExtendedASCIIText() {
-	cp := "\x80\xf7\xbe dollar"
-	str := cp1252ToUTF8(cp)
-	fmt.Printf("%q\n", str)
-}
+func ExtendedASCIIText(sample string) {
+	fmt.Printf( Sekvens)
 
-func init() {
-	for i, r := range cp1252 {
-		if r == 0 {
-			cp1252[i] = rune(i)
-		}
-	}
-}
-
-// cp1252 to Unicode table
-// ftp://ftp.unicode.org/Public/MAPPINGS/VENDORS/MICSFT/WINDOWS/CP1252.TXT
-var cp1252 = [256]rune{
-	0x80: '\u20AC', // EURO SIGN
-	0x81: '\uFFFD', // UNDEFINED
-	0x82: '\u201A', // SINGLE LOW-9 QUOTATION MARK
-	0x83: '\u0192', // LATIN SMALL LETTER F WITH HOOK
-	0x84: '\u201E', // DOUBLE LOW-9 QUOTATION MARK
-	0x85: '\u2026', // HORIZONTAL ELLIPSIS
-	0x86: '\u2020', // DAGGER
-	0x87: '\u2021', // DOUBLE DAGGER
-	0x88: '\u02C6', // MODIFIER LETTER CIRCUMFLEX ACCENT
-	0x89: '\u2030', // PER MILLE SIGN
-	0x8A: '\u0160', // LATIN CAPITAL LETTER S WITH CARON
-	0x8B: '\u2039', // SINGLE LEFT-POINTING ANGLE QUOTATION MARK
-	0x8C: '\u0152', // LATIN CAPITAL LIGATURE OE
-	0x8D: '\uFFFD', // UNDEFINED
-	0x8E: '\u017D', // LATIN CAPITAL LETTER Z WITH CARON
-	0x8F: '\uFFFD', // UNDEFINED
-	0x90: '\uFFFD', // UNDEFINED
-	0x91: '\u2018', // LEFT SINGLE QUOTATION MARK
-	0x92: '\u2019', // RIGHT SINGLE QUOTATION MARK
-	0x93: '\u201C', // LEFT DOUBLE QUOTATION MARK
-	0x94: '\u201D', // RIGHT DOUBLE QUOTATION MARK
-	0x95: '\u2022', // BULLET
-	0x96: '\u2013', // EN DASH
-	0x97: '\u2014', // EM DASH
-	0x98: '\u02DC', // SMALL TILDE
-	0x99: '\u2122', // TRADE MARK SIGN
-	0x9A: '\u0161', // LATIN SMALL LETTER S WITH CARON
-	0x9B: '\u203A', // SINGLE RIGHT-POINTING ANGLE QUOTATION MARK
-	0x9C: '\u0153', // LATIN SMALL LIGATURE OE
-	0x9D: '\uFFFD', // UNDEFINED
-	0x9E: '\u017E', // LATIN SMALL LETTER Z WITH CARON
-	0x9F: '\u0178', // LATIN CAPITAL LETTER Y WITH DIAERESIS
 }
 
 
