73aa60dfa108c0a5b122ae605ba84cc9d0636ac4
Julie Hodne Gundersen <jhg1997@gmail.com>
2018-02-14 16:25:54 +0100

    Julie

--- Main/Main.go
+++ Main/Main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"../OppgaveFire"
-	"os"
-	"os/signal"
-	"syscall"
-	"fmt"
+
 )
 
 func main() {
-	ascii.IterateOverExtendedASCIIStringLiteral(ascii.Ascii )
-	ascii.ExtendedASCIIText()
+	ascii.IterateOverASCIIStringLiteral(ascii.Ascii )
+	ascii.ExtendedASCIIText(ascii.Sekvens)
 }
+
