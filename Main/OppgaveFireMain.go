be22f5a76ba891033cb35aab615d1b0207dc098a
Julie Hodne Gundersen <jhg1997@gmail.com>
2018-03-01 13:15:05 +0100

    Julie

---
+++ Main/OppgaveFireMain.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"../OppgaveFire"
+)
+
+func main() {
+	ascii.IterateOverASCIIStringLiteral(ascii.Ascii )
+	ascii.ExtendedASCIIText(ascii.Sekvens)
+	//ascii.ExtendedASCIITextTest()
+
+}
+
---
+++ Oblig2/fileinfo.go
@@ -0,0 +1,1 @@
+package Oblig2
