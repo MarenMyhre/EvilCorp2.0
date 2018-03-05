fb864ee8528d3de20c32e703fd2e13bae43e215e
Katrine Skomedal <katris17@uia.no>
2018-03-05 12:41:12 +0100

    Katrine Skomedal

--- Oblig2/src/oppgave2/filecount.go
+++ Oblig2/src/oppgave2/filecount.go
@@ -1,8 +1,54 @@
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+	"log"
+	"sort"
+)
+
+var characterCount map[byte]int
 
 func main() {
-	fmt.Println("Programmet skal lese en tekst-fil, skrive ut totalt antall linjer og antall for fem runes som forekommer hyppigst i filen")
+	fi, err := ioutil.ReadFile("Oblig2/src/oppgave2/text.txt")
+	fileString := string(fi)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Number of lines in file: ")
+	fmt.Println(strings.Count(fileString, "\n"))
+
+	characterCount = make(map[byte]int)
+	for i := 0; i < len(fileString); i++ {
+		if _, ok := characterCount[fileString[i]]; ok {
+
+		} else {
+			characterCount[fileString[i]] = strings.Count(fileString, string(fileString[i]))
+		}
+	}
+
+	type kv struct {
+		Key byte
+		Value int
+	}
+
+	var ss []kv
+	for k, v := range characterCount {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value > ss[j].Value
+	})
+
+	counter := 0
+	for _, kv := range ss {
+		if counter < 5 {
+			fmt.Printf("%d. Rune: %s, counts: %d\n", counter + 1, string(kv.Key), kv.Value)
+		}
+		counter++
+	}
 }
