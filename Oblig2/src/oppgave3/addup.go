aef94183ec6edd43c5f95684d90aed75463abba9
Katrine Skomedal <katris17@uia.no>
2018-03-08 15:56:36 +0100

    Katrine Skomedal

--- Oblig2/src/oppgave3/addup.go
+++ Oblig2/src/oppgave3/addup.go
@@ -1,12 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"strconv"
+	"fmt"
+	"log"
+	"os/signal"
+)
 
 func main() {
-	fmt.Println("Legg sammen to tall fra terminal og summer i en annen funksjon")
-	fmt.Println(addUp(10, 10))
+	d := make(chan os.Signal, 2)
+	signal.Notify(d, os.Interrupt,)
+	go func() {
+		<-d
+		fmt.Println("End of process")
+		os.Exit(1)
+	}()
+
+	functionA()
 }
 
-func addUp(tall, tall2 int) int{
-	return tall + tall2
+func functionA() {
+	input1 := os.Args[1]
+	input2 := os.Args[2]
+
+	tallet1, err := strconv.Atoi(input1)
+	//Feilmeldingen "Invalid syntax" vises når den første påstanden ikke er et tall
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tallet2, err := strconv.Atoi(input2)
+	//Feilmeldingen "Invalid syntax" vises når den andre påstanden ikke er et tall
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := make(chan int)
+
+	go functionB(tallet1, tallet2, c)
+
+	resultat := <-c
+
+	fmt.Println(resultat)
+}
+
+func functionB(tallet1 int, tallet2 int, c chan int) {
+	resultat := tallet1 + tallet2
+
+	c <- resultat
 }
