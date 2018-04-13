2d169f34ce8a30369882267b3e13959f296c56f5
Katrine Skomedal <katris17@uia.no>
2018-04-13 15:29:46 +0200

    Katrine Skomedal

--- Oblig3/src/Oppgave3/udp/udp-client.go
+++ Oblig3/src/Oppgave3/udp/udp-client.go
@@ -1,24 +1,29 @@
-package udp
+package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"os"
 )
 
+func CheckError(err error) {
+	if err  != nil {
+		fmt.Println("Error: " , err)
+	}
+}
+
 func main() {
-	// connect to this socket
-	conn, _ := net.Dial("udp", "localhost:8080")
-	for {
-		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Text to send: ")
-		text, _ := reader.ReadString('\n')
-		// send to socket
-		fmt.Fprintf(conn, text, "\n")
-		// listen for reply
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: ", message)
+	ServerAddr,err := net.ResolveUDPAddr("udp","CHANGEME:27015")
+	CheckError(err)
+
+	LocalAddr, err := net.ResolveUDPAddr("udp", "192.168.1.129:0")
+	CheckError(err)
+
+	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	CheckError(err)
+
+	defer Conn.Close()
+	Conn.Write([]byte("Mote Fr 5.5 14:45 Flaaklypa"))
+	if err != nil {
+		fmt.Println(err)
 	}
 }
