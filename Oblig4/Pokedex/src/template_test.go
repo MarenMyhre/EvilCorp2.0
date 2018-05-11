bcc133d187f89936d3ca15c082c6b7abd7440f70
SophieKleiva <35769007+SophieKleiva@users.noreply.github.com>
2018-05-11 17:28:13 +0200

    Sophie

---
+++ Oblig4/Pokedex/src/template_test.go
@@ -0,0 +1,32 @@
+package main_test
+
+import (
+	"os"
+	"testing"
+	"path"
+)
+
+func Exists(fil string) bool {
+	if _, err := os.Stat(fil); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func TestFil(t *testing.T){
+	fp := path.Join("Template", "Start.html")
+	r := Exists(fp)
+	if r == false {
+		t.Error("File not found")
+	}
+}
+
+func TestFil2(t *testing.T){
+	fp2 := path.Join("Template", "Dex.html")
+	r := Exists(fp2)
+	if r == false {
+		t.Error("File not found")
+	}
+}
